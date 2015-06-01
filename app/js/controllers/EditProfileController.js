64e3fd8347d84fb2dab89653d5631096c706bbbd
Lucas de Castro Oliveira <lcastro.oliveira@acm.org>
2015-06-01 16:59:52 -0300

    Created Edit profile controller

---
+++ app/js/controllers/EditProfileController.js
@@ -0,0 +1,20 @@
+'use strict';
+
+eventsApp.controller('EditProfileController', 
+	function EditProfileController($scope) {
+		$scope.user = {};
+		
+		$scope.getGravatarUrl = function(email) {
+			var defaultGravatarUrl = "http://www.gravatar.com/avatar/000?s=200";
+			
+			var regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+			
+			if(!regex.test(email))
+				return defaultGravatarUrl;	
+			
+			return 'http://www.gravatar.com/avatar/'+md5(email)+".jpg?s=200&r=g";
+			
+		}	
+	}
+);
+
--- app/js/services/eventData.js
+++ app/js/services/eventData.js
@@ -1,48 +1,43 @@
 'use strict';
 
-eventsApp.factory('eventData', function (eventResource, authService) {
+eventsApp.factory('eventData',function() {
     return {
-        getEvent: function(eventId, callback) {
-            return eventResource.get({id:eventId}, function(event) {
-                if (callback)
-                    callback(event);
-            });
-        },
-        getAllEvents: function(callback) {
-            return eventResource.queryAll(callback);
-        },
-        getNextSessionId:function (event) {
-            var max = 0;
-            for (var idx = 0; idx < event.sessions.length; idx++) {
-                if (event.sessions[idx].id > max) {
-                    max = event.sessions[idx].id;
+       event : {
+            name : 'Angular Boot Camp',
+            date : 1359781015626,
+            time : '10:30 am',
+            location : {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl : '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name : 'Directives Masterclass introductory',
+                    creatorName : 'Bob Smith',
+                    duration: 1,
+                    level : 'Advanced',
+                    abstract : 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount : 0
+                },
+                {
+                    name : 'Scopes for fun and profit',
+                    creatorName : 'John Doe',
+                    duration: 2,
+                    level : 'Introductory',
+                    abstract : 'This session will take a closer look at scopes. Learn what they do it, and how to get them to do it for you.',
+                    upVoteCount : 0
+                },
+                {
+                    name : 'Well behaved controllers',
+                    creatorName : 'Jane Doe',
+                    duration: 4,
+                    level : 'Intermediate',
+                    abstract : 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.',
+                    upVoteCount : 0
                 }
-            }
-            return max+1;
-        },
-        save: function(event, callback) {
-            if (event.id) {
-                eventResource.save(event, function() { if (callback) callback(); });
-            } else {
-                eventResource.queryAll(function(events) {
-                    event.creator = authService.getCurrentUserName();
-                    event.id = getNextEventId(events);
-                    event.sessions = [];
-                    eventResource.save(event);
-                    if (callback)
-                        callback();
-                });
-            }
-        }
+            ]
+        }     
     };
-
-    function getNextEventId(events) {
-        var max = 0;
-        for (var idx = 0; idx < events.length; idx++) {
-            if (events[idx].id > max) {
-                max = events[idx].id;
-            }
-        }
-        return max+1;
-    }
 });
